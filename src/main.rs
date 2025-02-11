f70dc91c750d0b7666dd052e4d6a6bde2fc91dcb
Joseph Tafese <jetafese@uwaterloo.ca>
2025-02-11 18:27:12 -0500

    chore: wip in finding todo panics

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // #![no_std]
 
 #![no_builtins]
-// #![no_std]
+#![no_std]
 #![no_main]
 // #![feature(default_alloc_error_handler)]
 
@@ -19,47 +19,42 @@ mod cranelift_codegen;
 // mod wasmtime_environ;
 mod regalloc2;
 
-// use seahorn_stubs::assert;
+use seahorn_stubs::{error, nondet_u32};
 
-extern "C" {
-    fn __VERIFIER_error() -> !;
-    fn __VERIFIER_assume(pred: i32);
-    fn __VERIFIER_nondet_u32() -> u32;
+// extern "C" {
+//     fn __VERIFIER_error() -> !;
+// //     fn __VERIFIER_assume(pred: i32);
+// //     fn __VERIFIER_nondet_u32() -> u32;
+// }
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+#[no_mangle]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    error();
 }
 
-// use core::panic::PanicInfo;
-
-// #[panic_handler]
-// #[no_mangle]
-// fn panic(_panic: &PanicInfo<'_>) -> ! {
+// fn nondet_u32() -> u32 {
 //     unsafe {
-//         __VERIFIER_error();
+//         __VERIFIER_nondet_u32()
 //     }
 // }
 
-fn nondet_u32() -> u32 {
-    unsafe {
-        __VERIFIER_nondet_u32()
-    }
-}
-
-fn error() -> ! {
-    unsafe {
-        __VERIFIER_error()
-    }
-}
+// fn error() -> ! {
+//     unsafe {
+//         __VERIFIER_error()
+//     }
+// }
 
 #[allow(unused)]
 #[no_mangle]
-pub extern fn main(_argc: i32, _argv: *const *const u8) -> u32 {
+pub extern fn main() {
     let isa_flags = cranelift_codegen::isa::x64::x64_settings::Flags::new();
     let shared_flags = cranelift_codegen::settings::Flags::new();
     let mut asm = isa::x64::asm::Assembler::new(shared_flags, isa_flags);
     let src = Reg(VReg::new(32, regalloc2::RegClass::Int));
     let dst = isa::x64::regs::scratch();
     asm.add_rr(src, Writable::from_reg(dst),masm::OperandSize::S32);
-    // assert!(asm.finalize(None).srclocs)
-    // assert(2 > 5);
-    error();
-    nondet_u32()
+    // error();
 }
