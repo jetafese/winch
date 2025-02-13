cf97cece4e8c38d6e00132b7383d970ff76d3558
Joseph Tafese <jetafese@uwaterloo.ca>
2025-02-13 06:08:42 -0500

    feat: add seahorn stubs

---
+++ src/seahorn-stubs/src/lib.rs
@@ -0,0 +1,39 @@
+#![no_std]
+
+extern "C" {
+    fn __VERIFIER_error() -> !;
+    fn __VERIFIER_assume(pred: i32);
+    fn __VERIFIER_assert(pred: i32);
+    fn __VERIFIER_nondet_u32() -> u32;
+    fn __VERIFIER_nondet_i32() -> i32;
+}
+
+pub fn nondet_u32() -> u32 {
+    unsafe {
+        __VERIFIER_nondet_u32()
+    }
+}
+
+pub fn nondet_i32() -> i32 {
+    unsafe {
+        __VERIFIER_nondet_i32()
+    }
+}
+pub fn assume(pred: bool) {
+    unsafe {
+        __VERIFIER_assume(pred as i32)
+    }
+}
+
+pub fn error() -> ! {
+    unsafe {
+        __VERIFIER_error()
+    }
+}
+
+pub fn assert(pred: bool) {
+    if !pred {
+        error()
+    }
+}
+
